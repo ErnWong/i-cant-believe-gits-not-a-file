a64e9548559cda72fecd7508fa227f49d60c82e4
Ernest Wong <sudo@ernestwong.nz>
2025-10-27 00:43:42 +1300

    Add nix script to build vsix package

--- flake.nix
+++ flake.nix
@@ -8,13 +8,57 @@
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import (inputs.nixpkgs) { inherit system; });
+        vsix = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
+          name = "vscode-icantbelievegit-${finalAttrs.version}.vsix";
+          pname = "vscode-icantbelievegit-vsix";
+          version = "0.0.1";
+          src = ./.;
+          npmDeps = pkgs.fetchNpmDeps {
+            name = "${finalAttrs.pname}-npm-deps";
+            inherit (finalAttrs) src;
+            hash = "sha256-Egx2+H0rqGuP4MjDx7V+C+632/sXFOE2lJPPiFJBZiw=";
+          };
+          nativeBuildInputs = [
+            pkgs.nodejs_24
+            pkgs.npmHooks.npmConfigHook
+            pkgs.vsce
+          ];
+          buildPhase = ''
+            runHook preBuild
+            vsce package
+            runHook postBuild
+          '';
+          installPhase = ''
+            runHook preInstall
+            cp ./icantbelievegit-$version.vsix $out
+            runHook postInstall
+          '';
+        });
+        extension = pkgs.vscode-utils.buildVscodeExtension (finalAttrs: {
+          pname = "vscode-icantbelievegit";
+          inherit (finalAttrs.src) version;
+          vscodeExtPublisher = "ErnWong";
+          vscodeExtName = "icantbelievegit";
+          vscodeExtUniqueId = "${finalAttrs.vscodeExtPublisher}.${finalAttrs.vscodeExtName}";
+          src = vsix;
+          unpackPhase = ''
+            runHook preUnpack
+            unzip $src
+            runHook postUnpack
+          '';
+        });
       in {
         devShell = pkgs.mkShell {
           buildInputs=[
             pkgs.nodejs_24
             pkgs.nodePackages.typescript
             pkgs.nodePackages.typescript-language-server
+            pkgs.vsce
           ];
+        };
+        packages = {
+          inherit vsix extension;
+          default = extension;
         };
       }
     );
